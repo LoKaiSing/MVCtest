35a24dbcdbad093b478d657a7fda23f426081606
cindy <cindy820218@gmail.com>
2020-10-22 22:26:47 +0800

    加入99歲限制，多國語言

--- MVCtest/Models/Person.cs
+++ MVCtest/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,10 @@ namespace MVCtest.Models
     public class Person
     {
         public int Id { get; set; }
+        [Required(ErrorMessageResourceName = "Person_Name_Required", ErrorMessageResourceType =typeof(Resource1))]
         public string Name { get; set; }
+        [Required(ErrorMessage ="請輸入年紀")]
+        [Range(18,99,ErrorMessage ="請輸入年紀必須介於{1} - {2}歲之間")]
         public int Age { get; set; }
     }
 }
