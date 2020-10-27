a28384956ff828bb030dfa35c1212a8d501dcd06
cindy <cindy820218@gmail.com>
2020-10-27 09:33:19 +0800

    新增DepartmentController 的 Edit動作

--- MVCtest/Controllers/DepartmentsController.cs
+++ MVCtest/Controllers/DepartmentsController.cs
@@ -28,10 +28,41 @@ namespace MVCtest.Controllers
             {
                 db.Department.Add(department);
                 db.SaveChanges();
-                
+
                 return RedirectToAction("Index");
             }
             ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName");
+            return View(department);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName");
+            var item = db.Department.Find(id.Value);
+
+            return View(item);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id,Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.Department.Find(id);
+
+                item.Budget = department.Budget;
+                item.Name = department.Name;
+                item.StartDate = department.StartDate;
+                item.InstructorID = department.InstructorID;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
             return View(department);
         }
     }
