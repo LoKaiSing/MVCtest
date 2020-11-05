b899dd65b69379ed128aae0636ba7c7c9900871b
cindy <cindy820218@gmail.com>
2020-11-05 16:19:30 +0800

    建立Department 模型類別的 Buddy Class

---
+++ MVCtest/Models/Department.Partial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCtest.Models
+{
+	[MetadataType(typeof(DepartmentMetadata))]
+	public partial class Department
+    {
+        public class DepartmentMetadata
+        {
+            [Required]
+            public int DepartmentID { get; set; }
+            [Required]
+            public string Name { get; set; }
+            [Required]
+            public decimal Budget { get; set; }
+            [Required]
+            public Nullable<System.DateTime> StartDate { get; set; }
+            [Required]
+            public Nullable<int> InstructorID { get; set; }
+            public byte[] RowVersion { get; set; }
+        }
+
+
+
+    }
+}
